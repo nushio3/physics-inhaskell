c5666e635b91d70832bf1a3288925eed88201f1e
Takayuki Muranushi <muranushi@gmail.com>
2013-01-12 10:51:51 +0900

    add some constant.

--- JKL/Physics/Constants.hs
+++ JKL/Physics/Constants.hs
@@ -1,1 +1,8 @@
 module JKL.Physics.Constants where
+
+import UnitTyped
+import UnitTyped.SI
+import UnitTyped.SI.Meta
+
+solarMass :: Value MassDimension (U Gram) Double
+solarMass =  3e30 *| kilo gram `as` gram
