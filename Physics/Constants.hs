73912a15e9de3c982f481553d9b4aa4f82901f05
Takayuki Muranushi <muranushi@gmail.com>
2013-01-15 18:44:30 +0900

    start testing physics.

--- Physics/Constants.hs
+++ Physics/Constants.hs
@@ -4,5 +4,10 @@ import UnitTyped
 import UnitTyped.SI
 import UnitTyped.SI.Meta
 
+-- | Sun is heavy!
+--
+-- >>> 1+1
+-- 2
+
 solarMass :: Value MassDimension (U Gram) Double
 solarMass =  3e30 *| kilo gram `as` gram
