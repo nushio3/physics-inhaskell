8d8ecc4b8ebec8fe4b395ea037733fda402409a8
Takayuki Muranushi <muranushi@gmail.com>
2013-01-20 20:40:16 +0900

    define the concept of Mass

--- Physics/NewtonMechanics/Concepts.hs
+++ Physics/NewtonMechanics/Concepts.hs
@@ -1,1 +1,17 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
 module Physics.NewtonMechanics.Concepts where
+
+import           Data.Dynamic
+import           Data.Object.Dynamic
+import           Data.Object.Dynamic.Type
+import           UnitTyped
+import           UnitTyped.SI hiding (Mass)
+import           UnitTyped.SI.Meta
+
+
+data Mass = Mass deriving Typeable
+instance (Objective o, UseReal o) => Member o Mass where
+  type ValType o Mass = Value MassDimension (U (Kilo Gram)) (UnderlyingReal o)
