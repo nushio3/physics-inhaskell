7e67ff02860daa7d919b1b2cb51eea5af9fd11a8
Takayuki Muranushi <muranushi@gmail.com>
2013-02-04 10:13:45 +0900

    new mode of indexing.

--- Astrophysics/ProtoplanetaryDisk/Model.hs
+++ Astrophysics/ProtoplanetaryDisk/Model.hs
@@ -10,18 +10,15 @@ mass,
 
 ) where
 
-
 import           System.IO.Unsafe(unsafePerformIO)
 import           Text.CSL(Reference)
 import           Text.CSL.Input.Identifier (readID)
 import           Text.CSL.Output.Haddock(citet, citeUrl)
 
 
-
-
 {- |
  Typical mass for ppd.
- $(citet ida2007) 
+ $(citet ida2007)
 -}
 mass :: Double
 mass = 42
@@ -41,5 +38,3 @@ ida2007 = unsafePerformIO $ readID "isbn:978-4-13-060749-0"
 
 idaLin2003 :: Either String Reference
 idaLin2003 = unsafePerformIO $ readID "arxiv:astro-ph/0312144"
-
-
