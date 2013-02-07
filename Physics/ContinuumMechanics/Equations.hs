c6014f5bf3b793c6617c2daeb39e9475ce33a0d0
Takayuki Muranushi <muranushi@gmail.com>
2013-02-07 17:22:28 +0900

    try remove embeddock dependencies.

--- Physics/ContinuumMechanics/Equations.hs
+++ Physics/ContinuumMechanics/Equations.hs
@@ -1,3 +1,9 @@
+{- | We give equations by their left hand sides. -}
+
 module Physics.ContinuumMechanics.Equations where
 
+import Numeric.Physical.VectorCalculus
 
+
+gravityPoisson gravitationalPotential density
+  = gravitationalPotential - density
