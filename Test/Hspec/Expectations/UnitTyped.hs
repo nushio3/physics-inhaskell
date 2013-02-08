7ee6586cbc883355bbd493afca69497a855a86cd
Takayuki Muranushi <muranushi@gmail.com>
2013-02-08 19:45:59 +0900

    order of magnitude test implemented.

--- Test/Hspec/Expectations/UnitTyped.hs
+++ Test/Hspec/Expectations/UnitTyped.hs
@@ -1,10 +1,26 @@
 module Test.Hspec.Expectations.UnitTyped (isOfOrderOf, isNegligible) where
 
-import UnitTyped ()
-import Test.HUnit.Lang (Assertion)
+import Prelude hiding (abs)
+import UnitTyped.Type (Convertible', MapEq, Value(..), (|<|),(|>|), (*|))
+import UnitTyped.NoPrelude (abs)
+import Test.HUnit.Lang (Assertion, assertFailure)
 
-isOfOrderOf :: (Ord f, Fractional f,Convertible' a b, Convertible' c d, MapEq c a) => 
+
+isOfOrderOf :: (Ord f, Show f, Fractional f,Convertible' a b, Convertible' c d, MapEq c a) => 
   Value a b f -> Value c d f -> Assertion
-isOfOrderOf = undefined
+isOfOrderOf x y 
+  | x' |<| 10 *| y' && x' |>| 0.1 *| y' = return ()
+  | otherwise                          = putStrLn $
+    "expected " ++ show x ++ " to be of order of " ++ show y 
+                ++ ", but it isn't."
+  where
+    x' = abs x
+    y' = abs y
 
-isNegligible = 42
+isNegligible  :: (Ord f, Show f, Fractional f,Convertible' a b, Convertible' c d, MapEq c a) => 
+  f -> Value a b f -> Value c d f -> Assertion
+isNegligible epsilon x y 
+  | abs x |<| epsilon *| abs y = return ()
+  | otherwise                          = putStrLn $
+    "expected " ++ show x ++ " to be smaller than " ++ show epsilon ++ " * " ++ show y 
+                ++ ", but it isn't."
