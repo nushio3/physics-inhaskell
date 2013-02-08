56ee0a35e752542ed348ccc91152e80c9bf0f4e8
Takayuki Muranushi <muranushi@gmail.com>
2013-02-08 15:03:00 +0900

    start converting hydrostatic equation to be unit polimorphyc.

--- Physics/ContinuumMechanics/Equations.hs
+++ Physics/ContinuumMechanics/Equations.hs
@@ -4,13 +4,13 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ImpredicativeTypes #-}
-{-# LANGUAGE OverlappingInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE LiberalTypeSynonyms #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverlappingInstances #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Physics.ContinuumMechanics.Equations where
 
@@ -32,12 +32,18 @@ import           UnitTyped.SI.Derived (Density, Pressure, GravitationalPotential
 
 
 gravityPoisson ::
-  (Fractional x 
+  (Fractional x
   , dimLen ~ LengthDimension
   , dimPot ~ '[ '(Time, NTwo), '(Length, PTwo)]
   , dimDen ~ Density
-  , dimZhz ~ '[ '(Time, NTwo)] 
-  
+  , dimZhz ~ '[ '(Time, NTwo)]
+
+  , Convertible' dimLen uniLen
+  , Convertible' dimPot uniPot
+  , Convertible' dimDen uniDen
+  , Convertible' dimZhz uniZhz
+  , Convertible' dimZhz uniZhz'
+
   , MapMerge dimLen dimLen  dimLen2
   , MapNeg dimLen2  dimLenNeg2
   , MapMerge dimPot dimLenNeg2 dimZhz
@@ -48,50 +54,54 @@ gravityPoisson ::
   , MapMerge  uniPot uniLenNeg2 uniZhz
   , MapMerge uniDen   '[ '(Second, NTwo), '(Meter, PThree), '((Kilo Gram), NOne) ] uniZhz'
 
-  , Convertible' dimLen uniLen
-  , Convertible' dimPot uniPot
-  , Convertible' dimDen uniDen
-  , Convertible' dimZhz uniZhz
-  , Convertible' dimZhz uniZhz'
-
    ) =>
- (forall s. AD.Mode s => 
-  Vec3 (Value dimLen uniLen (AD s x)) 
+ (forall s. AD.Mode s =>
+  Vec3 (Value dimLen uniLen (AD s x))
        -> Value dimPot uniPot (AD s x))
 
-  -> (Vec3 (Value dimLen uniLen x) -> (Value dimDen uniDen x)) 
+  -> (Vec3 (Value dimLen uniLen x) -> (Value dimDen uniDen x))
 
-  -> (Vec3 (Value dimLen uniLen x) -> (Value dimZhz uniZhz x)) 
+  -> (Vec3 (Value dimLen uniLen x) -> (Value dimZhz uniZhz x))
 
 gravityPoisson gravitationalPotential density r
   = laplacian gravitationalPotential r |-| (4 *| pi |*| density r |*| g)
 
 
 
-hydrostatic :: forall x .(Fractional x) =>
- (forall s. AD.Mode s => 
- Vec3 ((AD s x):| Meter) -> Value Pressure (U Pascal) (AD s x))
- -> (Vec3 (x :| Meter) -> (Value Density '[ '(Meter, NThree),  '(Kilo Gram, POne) ] x)) 
- -> (Vec3 (x :| Meter) -> Vec3 (Value Acceleration '[ '(Second, NTwo), '(Meter, POne)] x)) 
- -> (Vec3 (x :| Meter) -> Vec3 (Value Acceleration '[ '(Second, NTwo), '(Meter, POne)] x)) 
+hydrostatic ::
+  ( Fractional x
+  , dimLen ~ LengthDimension
+  , dimPre ~ Pressure
+  , dimDen ~ Density
+  , dimAcc ~ Acceleration
+  , uniLen ~ U Meter
+  , uniPre ~ U Pascal
+  , uniDen ~ '[ '(Meter, NThree),  '(Kilo Gram, POne) ]
+  , uniAcc ~ '[ '(Second, NTwo), '(Meter, POne)]
+  ) =>
+ (forall s. AD.Mode s =>
+ Vec3 (Value dimLen uniLen (AD s x)) -> Value dimPre uniPre (AD s x))
+ -> (Vec3 (Value dimLen uniLen x) -> Value dimDen uniDen x)
+ -> (Vec3 (Value dimLen uniLen x) -> Vec3 (Value dimAcc uniAcc x))
+ -> (Vec3 (Value dimLen uniLen x) -> Vec3 (Value dimAcc uniAcc x))
 hydrostatic pressure density acceleration r
-  = compose $ \i -> 
+  = compose $ \i ->
       let ret = (to ret) $ (gradP r ! i) |/| (density r) |+| (acceleration r ! i)
       in ret
-  where 
-    gradP =  grad pressure 
+  where
+    gradP =  grad pressure
 
 
 --     gradP ::
 --         Vec3 (x :| Meter) ->
 --         Vec3 (Value  '[ '(Time, NTwo), '(Mass, POne), '(Length, NTwo) ]
 --                      '[ '(Second, NTwo), '(Kilo Gram, POne), '(Meter, NTwo) ]
---                        x)        
+--                        x)
 
 
 -- This bumps into an overlapping instance. cannot be implemented generically.
 
--- gravityPoisson :: 
+-- gravityPoisson ::
 --   forall x dimX uniX dimX2 uniX2 negDimX2 negUniX2 dimD uniD dimY uniY dimY' uniY' dimY'' uniY''.
 --          (Fractional x,
 --            Convertible' dimX uniX,
@@ -103,7 +113,7 @@ hydrostatic pressure density acceleration r
 --            MapEq dimD Density,
 --            MapEq dimY GravitationalPotential,
 --            MapEq dimY' '[ '(Time, NTwo)],
--- 
+--
 --            MapMerge dimX dimX dimX2,
 -- --            MapMerge uniX uniX uniX2,
 --            MapNeg  dimX2 negDimX2,
@@ -112,12 +122,11 @@ hydrostatic pressure density acceleration r
 -- --            MapMerge uniY negUniX2 uniY'
 --            MapMerge dimD '[ '(Time, NTwo), '(Length, PThree), '(Mass, NOne) ] uniY'' ,
 --            MapMerge uniD '[ '(Second, NTwo), '(Meter, PThree), '((Kilo Gram), NOne) ] uniY''
---           ) => 
+--           ) =>
 --   (forall s. AD.Mode s => Vec3 (Value dimX uniX (AD s x)) -> Value dimY uniY (AD s x))
---   -> (Vec3 (Value dimX uniX x) -> (Value dimD uniD x)) 
---   -> (Vec3 (Value dimX uniX x) -> (Value dimY' uniY' x)) 
+--   -> (Vec3 (Value dimX uniX x) -> (Value dimD uniD x))
+--   -> (Vec3 (Value dimX uniX x) -> (Value dimY' uniY' x))
 -- gravityPoisson gravitationalPotential density r
---   = laplacian gravitationalPotential r |-| 
+--   = laplacian gravitationalPotential r |-|
 --     (g |*| density r :: Value dimY'' uniY'' x)
 --     --(4 *| pi |*| density r |*| g)
-
